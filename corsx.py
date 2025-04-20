83d2244c72c773187109e4ad12b7253d608ce97e
SkyLing <70288759+SkyLingRQ@users.noreply.github.com>
2025-04-20 19:53:02 -0300

    Update corsx.py

--- corsx.py
+++ corsx.py
@@ -1,46 +1,27 @@
-import argparse
-import aiohttp
 import asyncio
-import urllib.parse
+import aiohttp
 from colorama import Fore, init
 
 init()
 
 rd = Fore.RED
-g = Fore.GREEN
 reset = Fore.RESET
+g = Fore.GREEN
 
-urlMal = "http://evil.com"
-headers = {
-        "Origin":urlMal
-}
 corsFound = []
 
-async def fetch(session, url):
-    url = url.strip()
-    try:
-        async with session.get(url, headers=headers) as r:
-            header_result = r.headers.get("Access-Control-Allow-Origin")
-            header_result2 = r.headers.get("Access-Control-Allow-Credentials")
-            if header_result == urlMal:
-                if header_result2 and r.headers[header_result2].lower() == "true":
-                    print(f"\n            {g}C O R S - S C A N\n{rd}+ {g}{url}\n{rd}+{g} Reflected Origin And Credentials Access Control Is True\n{rd}+{g} Critical{reset}\n")
-                else:
-                    print(f"\n            {g}C O R S - S C A N\n{rd}+ {g}{url}\n{rd}+{g} Reflected Origin\n{rd}+{g} Hight{reset}\n")
-            else:
-                print(f"\n            {g}C O R S - S C A N\n\n{rd}        ------CORS not found-----{reset}\n")
-    except Exception:
-            pass
-
-async def fetch2(session, url):
+async def scan_cors(url, session):
+    urlMal = "http://evil.com"
+    headers = {
+        "Origin":urlMal
+    }
     url = url.strip()
     try:
         async with session.get(url, headers=headers) as resp:
             header_result = resp.headers.get("Access-Control-Allow-Origin")
             header_result2 = resp.headers.get("Access-Control-Allow-Credentials")
-
-            if header_result == urlMal:
-                if header_result2 and resp.headers[header_result2].lower() == "true":
+            if header_result == urlMal and resp.status != 404:
+                if header_result2 and header_result2.lower() == "true":
                     print(f"\n            {g}C O R S - S C A N\n{rd}+ {g}{url}\n{rd}+{g} Reflected Origin And Credentials Access Control Is True\n{rd}+{g} Critical{reset}\n")
                 else:
                     print(f"\n            {g}C O R S - S C A N\n{rd}+ {g}{url}\n{rd}+{g} Reflected Origin\n{rd}+{g} Hight{reset}\n")
@@ -49,25 +30,10 @@ async def fetch2(session, url):
                 pass
     except Exception:
         pass
-
-
-async def main():
-    parse = argparse.ArgumentParser(description="C O R S - EviLight")
-    parse.add_argument("-u", "--url", help="Scanning a URL")
-    parse.add_argument("-f", "--file", help="Scanning using files with URLs")
-    args = parse.parse_args()
-
-    if args.url:
-        async with aiohttp.ClientSession() as session:
-            await fetch(session, args.url)
-    
-    if args.file:
-        with open(args.file, "r") as file:
-            urls = file.readlines()
-
-        async with aiohttp.ClientSession() as session:
-            task = [fetch2(session, url) for url in urls]
-            await asyncio.gather(*task)
-            print(f"\n{g}[+] {len(corsFound)} Cors vulnerability found{reset}\n")
-
-asyncio.run(main())
+async def main(file):
+    with open(file, 'r') as rf:
+        urls = rf.readlines()
+    async with aiohttp.ClientSession() as session:
+        task = [scan_cors(url, session) for url in urls]
+        await asyncio.gather(*task)
+        print(f"\n{g}[+] {len(corsFound)} Cors vulnerability found{reset}\n")
